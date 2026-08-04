68e41101c4514fb63fd28c888c8f2902eb1baa76
Aliaqa Alizada <alimhd276@gmail.com>
2026-08-04 20:22:58 +0200

    refactore _home.ts temaplate in separate file

--- src/typescript/components/_home.ts
+++ src/typescript/components/_home.ts
@@ -1,34 +1,9 @@
 import { renderSettings } from "./settings/_settings";
+import { homeTemplate } from "./templates/_home-template";
 
 export function renderHome(app: HTMLElement): void {
-    app.innerHTML = `
-        <section class="screen home">
+    app.innerHTML = homeTemplate();
 
-            <header class="home__header">
-                <p>it's play time.</p>
-                <h1>Ready to play?</h1>
-            </header>
-
-            <section class="home__content">
-
-                <button id="playButton" class="btn">
-                    <img class="game-icon" src="src/assets/images/home/stadia_controller_s.svg" alt="Stadia_controller-icon">
-                     Play
-                    <img class="arrow-icon" src="src/assets/images/home/Arrow 1.svg" alt="arrow-icon">
-                    
-                </button>
-           
-
-            </section>
-            
-            <img class="stadia_controller" src="src/assets/images/home/Stadia_controller.svg" alt="Stadia_controller">
-
-            <footer class="home__footer">
-                <p>© Memory Game 2026</p>
-            </footer>
-
-        </section>
-    `;
     const playButton = app.querySelector("#playButton");
     playButton?.addEventListener("click", () => {
         renderSettings(app);
---
+++ src/typescript/components/templates/_home-template.ts
@@ -0,0 +1,29 @@
+export function homeTemplate(): string {
+    return `
+        <section class="screen home">
+
+            <header class="home__header">
+                <p>it's play time.</p>
+                <h1>Ready to play?</h1>
+            </header>
+
+            <section class="home__content">
+                <button id="playButton" class="btn">
+                    <img class="game-icon" src="src/assets/images/home/stadia_controller_s.svg" alt="Stadia_controller-icon">
+                     Play
+                    <img class="arrow-icon" src="src/assets/images/home/Arrow 1.svg" alt="arrow-icon">
+                    
+                </button>
+            </section>
+            
+            <img class="stadia_controller" src="src/assets/images/home/Stadia_controller.svg" alt="Stadia_controller">
+
+            <footer class="home__footer">
+                <p>© Memory Game 2026</p>
+            </footer>
+
+        </section>
+    
+    `;
+
+}
